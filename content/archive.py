809fd7402a457a032442b312510f47cecc16e170
Grishanya030 <102467977+Grishanya030@users.noreply.github.com>
2025-05-09 00:07:04 +0300

    Улучшение структуры

--- content/archive.py
+++ content/archive.py
@@ -1,8 +1,22 @@
 import os
+import re
 
 # Путь к директории, содержащей .md файлы
-root_dir = "D:\Github\Shards-of-Eternia\content"
+
+root_dir = r"D:\Github\Shards-of-Eternia\content"
 output_file = "собранный_сеттинг.md"
+print("Введите путь к папке, если хотите архивировать всю папку, то просто нажмите Enter")
+
+tmp_dir=input();
+if(tmp_dir != ""):
+    root_dir=tmp_dir
+
+
+
+
+# Функция для натуральной сортировки
+def natural_key(string):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', string)]
 
 def read_file_with_fallback(path):
     try:
@@ -17,13 +31,21 @@ def read_file_with_fallback(path):
             return None
 
 with open(output_file, "w", encoding="utf-8") as outfile:
+    # Справка
+    outfile.write("**Справка**\n\n")
+    outfile.write("Этот документ представляет собой объединённый файл, собранный из всех Markdown-файлов (.md) внутри директории `content` и её поддиректорий.\n")
+    outfile.write("Каждый файл отмечен заголовком вида: `файл *относительный_путь_файла*`\n\n")
+    outfile.write("---\n\n")
+
     for foldername, subfolders, filenames in os.walk(root_dir):
+        subfolders.sort(key=natural_key)
+        filenames.sort(key=natural_key)
         for filename in filenames:
             if filename.lower().endswith(".md"):
                 file_path = os.path.join(foldername, filename)
                 relative_path = os.path.relpath(file_path, root_dir)
                 content = read_file_with_fallback(file_path)
                 if content is not None:
-                    outfile.write(f"# {relative_path}\n\n")
+                    outfile.write(f"файл *{relative_path}*\n\n")
                     outfile.write(content + "\n\n")
 print('готово')
